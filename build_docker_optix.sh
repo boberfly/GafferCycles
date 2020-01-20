4912447abc5c128f2355d1731138226e9642aebf
Alex Fuller <boberfly@gmail.com>
2020-01-19 23:37:51 -0800

    Update build scripts

--- build_docker_optix.sh
+++ build_docker_optix.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python build.py --version 0.13.0 --cyclesVersion 0.13.0 --optix 1 --docker 1 --experimental 0 --upload 0 $@
+python build.py --version 0.15.0 --cyclesVersion 0.15.0 --optix 1 --docker 1 --experimental 0 --upload 0 $@
