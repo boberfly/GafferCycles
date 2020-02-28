7e71d8337fec14aef533c6f2d23c1d2b3a095e6a
Alex Fuller <boberfly@gmail.com>
2020-02-28 00:29:50 -0800

    Version bump.

--- build_docker_optix.sh
+++ build_docker_optix.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python build.py --version 0.16.0 --cyclesVersion 0.16.0 --optix 1 --docker 1 --experimental 0 --upload 0 $@
+python build.py --version 0.18.0 --cyclesVersion 0.17.0 --optix 1 --docker 1 --experimental 0 --upload 0 $@
