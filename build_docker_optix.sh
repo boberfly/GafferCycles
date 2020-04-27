f76fac9a77beb8cb6f78cc9da76578b4ca889289
Alex Fuller <boberfly@gmail.com>
2020-04-26 19:54:27 -0700

    Setting up for 0.20.0

--- build_docker_optix.sh
+++ build_docker_optix.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python build.py --version 0.19.0 --cyclesVersion 0.19.0 --optix 1 --docker 1 --experimental 0 --upload 0 $@
+python build.py --version 0.20.0 --cyclesVersion 0.20.0 --optix 1 --docker 1 --experimental 0 --upload 0 $@
